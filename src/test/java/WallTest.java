ed929da748b9a361af1f5f3306c8a4f2f82adb1a
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-07-10 20:51:24 +0200

    Create tests for findBlockByColor method

--- src/test/java/WallTest.java
+++ src/test/java/WallTest.java
@@ -1,2 +1,54 @@
+import org.example.Block;
+import org.example.Wall;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class WallTest {
+    private Wall wall;
+    @BeforeEach
+    public void setUp() {
+        wall = new Wall();
+    }
+
+    private List<Block> getSampleBlocks() {
+        return Arrays.asList(
+                Wall.createBlock("red", "brick"),
+                Wall.createBlock("red", "brick"),
+                Wall.createBlock("red", "wood"),
+                Wall.createBlock("blue", "wood")
+        );
+    }
+
+    @Test
+    public void testFindBlockByColor_ExistingColor_ReturnBlock(){
+        // Arrange
+        List<Block> blocks = getSampleBlocks();
+        wall.addBlocks(blocks);
+
+        // Act
+        Optional<Block> foundBlockByColor = wall.findBlockByColor("red");
+
+        // Assert
+        Assertions.assertTrue(foundBlockByColor.isPresent());
+        Assertions.assertEquals("red", foundBlockByColor.get().getColor());
+    }
+
+    @Test
+    public void testFindBlockByColor_NonExistingColor_ReturnEmptyOptional(){
+        // Arrange
+        List<Block> blocks = getSampleBlocks();
+        wall.addBlocks(blocks);
+
+        // Act
+        Optional<Block> foundBlockByColor = wall.findBlockByColor("green");
+
+        // Assert
+        Assertions.assertTrue(foundBlockByColor.isEmpty());
+    }
+
 }
