257ee17d67ad01f211970b19e1f1912e30e95ab0
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-07-10 19:56:57 +0200

    Create sample data and prints in main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,28 @@
 package org.example;
 
+import java.util.List;
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
+        Wall wall = new Wall();
+
+        wall.addBlock(Wall.createBlock("red", "brick"));
+        wall.addBlock(Wall.createBlock("red", "brick"));
+        wall.addBlock(Wall.createBlock("red", "wood"));
+        wall.addBlock(Wall.createBlock("blue", "wood"));
+
+        Optional<Block> foundBlockByColor = wall.findBlockByColor("grey");
+        if (foundBlockByColor.isPresent()) {
+            System.out.println("Found block: " + foundBlockByColor.get().getColor());
+        } else {
+            System.out.println("Block not found");
+        }
+
+        List<Block> foundBlocksByMaterial = wall.findBlocksByMaterial("wood");
+        System.out.println("Found blocks: " + foundBlocksByMaterial.size());
+
+        int totalBlocks = wall.count();
+        System.out.println("Total blocks: " + totalBlocks);
     }
 }
