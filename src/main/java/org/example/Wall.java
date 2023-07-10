966d78dd527b432705b276cfec6176ecfc02adda
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-07-10 19:04:58 +0200

    Implementation of findBlocksByColor method using for loop

--- src/main/java/org/example/Wall.java
+++ src/main/java/org/example/Wall.java
@@ -1,13 +1,23 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class Wall implements Structure, CompositeBlock {
     private List<Block> blocks;
 
+    public Wall() {
+        this.blocks = new ArrayList<>();
+    }
+
     @Override
     public Optional<Block> findBlockByColor(String color) {
+        for (Block block : blocks) {
+            if (block.getColor().equals(color)) {
+                return Optional.of(block);
+            }
+        }
         return Optional.empty();
     }
 
